92546ec2281308ace217d775385ee28bb96d1b5a
achrafzarouki <achrafzarouki20@gmail.com>
2025-04-26 15:23:15 +0100

    I created a custom provider

---
+++ src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,37 @@
+package com.eazybytes.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    public final UserDetailsService userDetailsService;
+    public final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if(passwordEncoder.matches(pwd,userDetails.getPassword())){
+            return new UsernamePasswordAuthenticationToken(username, pwd , userDetails.getAuthorities());
+        }else{
+            throw new BadCredentialsException("Invalid  pwd");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
